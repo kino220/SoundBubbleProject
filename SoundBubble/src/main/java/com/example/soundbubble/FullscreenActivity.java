93e9bc06c320a281ce951f7c218d534460588efb
kino220 <m.j.1213.bad@gmail.com>
2013-11-20 18:32:38 +0900

    test

--- SoundBubble/src/main/java/com/example/soundbubble/FullscreenActivity.java
+++ SoundBubble/src/main/java/com/example/soundbubble/FullscreenActivity.java
@@ -4,13 +4,21 @@ import com.example.soundbubble.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -18,7 +26,8 @@ import android.widget.TextView;
  *
  * @see SystemUiHider
  */
-public class FullscreenActivity extends Activity {
+@TargetApi(Build.VERSION_CODES.FROYO)
+public class FullscreenActivity extends Activity implements RecognitionListener {
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -46,6 +55,9 @@ public class FullscreenActivity extends Activity {
      * The instance of the {@link SystemUiHider} for this activity.
      */
     private SystemUiHider mSystemUiHider;
+
+    private SpeechRecognizer mSpeechRecognizer;
+    private String LOGTAG = "SpeechRecognizerTest";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -107,6 +119,13 @@ public class FullscreenActivity extends Activity {
                 } else {
                     mSystemUiHider.show();
                 }
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+                intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,
+                        getPackageName());
+                mSpeechRecognizer.startListening(intent);
             }
         });
 
@@ -115,6 +134,12 @@ public class FullscreenActivity extends Activity {
         textView.setText(str);
         FrameLayout mainLayout = (FrameLayout)findViewById(R.id.main_layout);
         mainLayout.addView(textView);
+
+
+        mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mSpeechRecognizer.setRecognitionListener(this);
+
+
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
@@ -162,5 +187,103 @@ public class FullscreenActivity extends Activity {
     private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+    // 音声認識準備完了
+    @Override
+    public void onReadyForSpeech(Bundle params) {
+        Toast.makeText(this, "音声認識準備完了", Toast.LENGTH_SHORT);
+        //iv.setImageResource(R.drawable.ready);
+    }
+
+    // 音声入力開始
+    @Override
+    public void onBeginningOfSpeech() {
+        Toast.makeText(this, "入力開始", Toast.LENGTH_SHORT);
+        //iv.setImageResource(R.drawable.listen);
+    }
+
+    // 録音データのフィードバック用
+    @Override
+    public void onBufferReceived(byte[] buffer) {
+        Log.v(LOGTAG,"onBufferReceived");
+    }
+
+    // 入力音声のdBが変化した
+    @Override
+    public void onRmsChanged(float rmsdB) {
+        Log.v(LOGTAG,"recieve : " + rmsdB + "dB");
+    }
+
+    // 音声入力終了
+    @Override
+    public void onEndOfSpeech() {
+        Toast.makeText(this, "入力終了", Toast.LENGTH_SHORT);
+        //iv.setImageResource(R.drawable.tb);
+    }
+
+    // ネットワークエラー又は、音声認識エラー
+    @Override
+    public void onError(int error) {
+        switch (error) {
+            case SpeechRecognizer.ERROR_AUDIO:
+                // 音声データ保存失敗
+                break;
+            case SpeechRecognizer.ERROR_CLIENT:
+                // Android端末内のエラー(その他)
+                break;
+            case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                // 権限無し
+                break;
+            case SpeechRecognizer.ERROR_NETWORK:
+                // ネットワークエラー(その他)
+                Log.e(LOGTAG, "network error");
+                break;
+            case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                // ネットワークタイムアウトエラー
+                Log.e(LOGTAG, "network timeout");
+                break;
+            case SpeechRecognizer.ERROR_NO_MATCH:
+                // 音声認識結果無し
+                Toast.makeText(this, "no match Text data", Toast.LENGTH_LONG);
+                break;
+            case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                // RecognitionServiceへ要求出せず
+                break;
+            case SpeechRecognizer.ERROR_SERVER:
+                // Server側からエラー通知
+                break;
+            case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                // 音声入力無し
+                Toast.makeText(this, "no input?", Toast.LENGTH_LONG);
+                break;
+            default:
+        }
+    }
+
+    // イベント発生時に呼び出される
+    @Override
+    public void onEvent(int eventType, Bundle params) {
+        Log.v(LOGTAG,"onEvent");
+    }
+
+    // 部分的な認識結果が得られる場合に呼び出される
+    @Override
+    public void onPartialResults(Bundle partialResults) {
+        Log.v(LOGTAG,"onPartialResults");
+    }
+
+    // 認識結果
+    @Override
+    public void onResults(Bundle results) {
+        ArrayList recData = results
+                .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+        String getData = new String();
+        for (Object s : recData) {
+            getData += s + ",";
+        }
+
+        Toast.makeText(this, getData, Toast.LENGTH_SHORT).show();
     }
 }
